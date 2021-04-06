76c446b9d1ce15c905cde318e5ddeefdbe2cad92
pandaren <panjianbo@juwang.cn>
2021-04-06 11:36:34 +0800

    初始化目录

---
+++ app/src/main/java/com/haochang/party/LogoActivity.kt
@@ -0,0 +1,11 @@
+package com.haochang.party
+
+import androidx.appcompat.app.AppCompatActivity
+
+/**
+ * @author: pandaren
+ * @createTime: 2021/4/2
+ * main logo
+ */
+class LogoActivity : AppCompatActivity() {
+}
