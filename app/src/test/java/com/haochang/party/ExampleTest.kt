c8d2e02c1dfac5e282b78a9c6400c36bc364934e
pandaren <panjianbo@juwang.cn>
2023-10-13 18:19:39 +0800

    配置调整

---
+++ app/src/test/java/com/haochang/party/ExampleTest.kt
@@ -0,0 +1,17 @@
+package com.haochang.party
+
+/**
+ * author:pandaren
+ * createDate:2023/9/25 15:33
+ */
+class ExampleTest {
+    inner class ClassA {
+        var paramA: String = ""
+        var paramB: String = ""
+    }
+
+
+    public fun addition_isCorrect() {
+        val classA = ClassA()
+    }
+}
